c49d43ba7702ee51f1209495b1a9dbef19e41a7f
Daniel Danis <daniel.danis@jax.org>
2020-08-05 17:00:13 +0200

    Add modules

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module xyz.ielis.modular_unit_testing.core {
+    requires xyz.ielis.modular_unit_testing.io;
+}
---
+++ io/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module xyz.ielis.modular_unit_testing.io {
+    exports xyz.ielis.modular_unit_testing.io;
+}
