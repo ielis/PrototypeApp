0fd263383ca2f488d7d58335e909b9b6fa2472c2
Daniel Danis <daniel.danis@jax.org>
2020-08-06 12:37:42 +0200

    Add spring-managed gui

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -1,6 +1,17 @@
 module xyz.ielis.modular_unit_testing.gui {
     requires xyz.ielis.modular_unit_testing.core;
-    requires javafx.controls;
+    requires xyz.ielis.modular_unit_testing.db;
 
-    exports xyz.ielis.modular_unit_testing.gui to javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires spring.context;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+
+    exports xyz.ielis.modular_unit_testing.gui to javafx.graphics, spring.context;
+    opens xyz.ielis.modular_unit_testing.gui to spring.core, spring.beans;
+
+    exports xyz.ielis.modular_unit_testing.gui.controller to javafx.fxml;
+    opens xyz.ielis.modular_unit_testing.gui.controller to javafx.fxml, spring.beans;
 }
--- gui/src/main/java/xyz/ielis/modular_unit_testing/gui/Main.java
+++ gui/src/main/java/xyz/ielis/modular_unit_testing/gui/Main.java
@@ -1,22 +1,47 @@
 package xyz.ielis.modular_unit_testing.gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import xyz.ielis.modular_unit_testing.gui.controller.MainController;
 
+@SpringBootApplication
 public class Main extends Application {
+
+    private ConfigurableApplicationContext ctx;
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        final String[] args = getParameters().getRaw().toArray(new String[]{});
+        ctx = new SpringApplicationBuilder()
+                .sources(Main.class)
+                .run(args);
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Label label = new Label("Hi there");
-        final Scene scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+        final FXMLLoader loader = new FXMLLoader(MainController.class.getResource("Main.fxml"));
+
+        loader.setControllerFactory(ctx::getBean);
+
+        final Parent parent = loader.load();
+        stage.setScene(new Scene(parent));
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop() throws Exception {
+        ctx.stop();
+        Platform.exit();
     }
 }
